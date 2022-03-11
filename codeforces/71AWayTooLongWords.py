e8f303ea0ad890c203f65e5668bd9bfb02e41b99
Taufiq63569 <taufiq63569@gmail.com>
2022-03-11 23:57:16 +0600

    71A slove

--- codeforces/71AWayTooLongWords.py
+++ codeforces/71AWayTooLongWords.py
@@ -1,1 +1,8 @@
 num = int(input())
+list =[]
+for i in range(num):
+    list.append(str(input()))
+    if(len(list[i])>10):
+        print(list[i][0]+str(len(list[i])-2)+list[i][len(list[i])-1])
+    else:
+        print(list[i])
