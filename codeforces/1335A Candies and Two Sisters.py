574903cdf6551f77f19a8d281a7523256120f28f
Neloy-SWE <taufiqneloy.swe@gmail.com>
2022-05-11 23:08:32 +0600

    1335A solved

---
+++ codeforces/1335A Candies and Two Sisters.py
@@ -0,0 +1,13 @@
+num1 = int(input())
+list1=[]
+result=0
+for i in range(num1):
+    list1.append(int(input()))
+    if list1[i]>2:
+        if list1[i]%2==1:
+            result=(list1[i]-1)/2
+        elif list1[i]%2==0:
+            result=(list1[i]-2)/2
+        print(result)
+    else:
+        print(0)
