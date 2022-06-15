31f0cb7e6b7fe2571834a50bc4e3ff6e993c66d2
Neloy-SWE <taufiqneloy.swe@gmail.com>
2022-06-15 14:57:11 +0600

    1370A solved

--- codeforces/test.py
+++ codeforces/test.py
@@ -3,14 +3,5 @@
 #     a = list(map(int, input().split()))
 
 
-for i in range(int(input())):
-    k = int(input())
-    a = list(map(int, input().split()))
-    b = list(map(int, input().split()))
-    minA = min(a)
-    minB = min(b)
-    count = 0
-    for i in range(k):
-        count+=max((a[i]-minA), (b[i]-minB))
-    print(count)
+
 
