52f08442de249df590c2a50d0e1a92d504126e6c
Neloy-SWE <taufiqneloy.swe@gmail.com>
2022-06-15 22:50:17 +0600

    promo solved

--- codeforces/Cont Promo.py
+++ codeforces/Cont Promo.py
@@ -1,12 +1,8 @@
 n, q = map(int, input().split())
 product = sorted(list(map(int, input().split())), reverse=True)
-
-xProduct = []
-xProduct.append(product[0])
-for i in range(1, n):
-    xProduct.append(xProduct[i-1]+product[i])
+xProduct = [0]
+for i in range(1, n+1):
+    xProduct.append(xProduct[i-1]+product[i-1])
 for j in range(q):
     x, y = map(int, input().split())
-    ans = 0
-    ans = xProduct[x-1]-xProduct[x-q]+product[x-q]
-    print(ans)
+    print(xProduct[x]-xProduct[x-y])
