849fbd58d47f79f1837a0b1a8487488dad671962
Neloy-SWE <taufiqneloy.swe@gmail.com>
2022-06-25 02:26:42 +0600

    1311A solved

--- codeforces/1311A Add Odd or Subtract Even.py
+++ codeforces/1311A Add Odd or Subtract Even.py
@@ -1,11 +1,3 @@
-# for i in range(int(input())):
-#     n = int(input())
-#     s = input()
-#     a, b = map(int, input().split())
-#     a, b, = map(str, input().split())
-#     a = list(map(int, input().split()))
-#     a = list(map(str, input().split()))
-
 for i in range(int(input())):
     a, b = map(int, input().split())
     if a==b:
