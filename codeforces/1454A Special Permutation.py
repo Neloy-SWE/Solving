a5e923e8d49bb1670a202eba0113ee3b104d4140
Neloy-SWE <taufiqneloy.swe@gmail.com>
2022-06-25 11:48:35 +0600

    1454A solved

---
+++ codeforces/1454A Special Permutation.py
@@ -0,0 +1,5 @@
+for i in range(int(input())):
+    n = int(input())
+    for i in range (2, n+1 ):
+        print(i, end=" ")
+    print(1)
--- codeforces/test.py
+++ codeforces/test.py
@@ -6,17 +6,3 @@
 #     a = list(map(int, input().split()))
 #     a = list(map(str, input().split()))
 
-for i in range(int(input())):
-    a, b = map(int, input().split())
-    if a==b:
-        print(0)
-    elif(a<b):
-        if((b-a)%2==1):
-            print(1)
-        else:
-            print(2)
-    elif(a>b):
-        if((b-a)%2==0):
-            print(1)
-        else:
-            print(2)
