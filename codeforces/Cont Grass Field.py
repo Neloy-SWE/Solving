aa8e9d97b0561382518b2bba5877daa7b54c5d46
Neloy-SWE <taufiqneloy.swe@gmail.com>
2022-07-08 22:22:49 +0600

    grass field solved

---
+++ codeforces/Cont Grass Field.py
@@ -0,0 +1,9 @@
+for i in range(int(input())):
+    a, b = map(int, input().split())
+    c, d = map(int, input().split())
+    if (a+b+c+d) == 0:
+        print(0)
+    elif(a+b+c+d) == 4:
+        print(2)
+    else:
+          print(1)
