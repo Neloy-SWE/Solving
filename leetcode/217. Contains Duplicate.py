6694ab323f782575edabca2c93ac24d5a9f718dd
Neloy-SWE <taufiqneloy.swe@gmail.com>
2023-09-06 10:43:29 +0600

    1. two sum solved

--- leetcode/217. Contains Duplicate.py
+++ leetcode/217. Contains Duplicate.py
@@ -1,8 +1,7 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         setA = set(nums)
-        if len(nums)==len(setA):
+        if len(nums) == len(setA):
             return False
         else:
             return True
-        
