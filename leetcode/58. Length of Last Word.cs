0ac36cd8e5ed5bad2bb0131b28b1a9ff0f6c64a5
Taufiq Ahommed Neloy <taufiqneloy.swe@gmail.com>
2024-12-17 22:45:34 +0600

    58. Length of Last Word

---
+++ leetcode/58. Length of Last Word.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int LengthOfLastWord(string s) {
+        int length = 0;
+        for (int i = s.Length-1; i>=0; i--){
+            if (s[i] != ' '){
+                length++;
+            }
+            else if (s[i] == ' ' && length>0){
+                break;
+            }
+        }
+        return length;
+    }
+}
